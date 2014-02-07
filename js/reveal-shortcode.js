8fabee08fd4c471e44d855fe3f3d97a1a40cfa76
mawaha <matthew@unknown-90-2b-34-dc-a5-6f.home>
2014-02-07 12:31:58 +0000

    Fixed jQuery conflict
    added IIFE for no conflict

--- js/reveal-shortcode.js
+++ js/reveal-shortcode.js
@@ -1,7 +1,9 @@
-$(function(){
+(function($){
+	$(function(){
 
-	var closingHTML = '<a class="close-reveal-modal">&#215;</a>';
+		var closingHTML = '<a class="close-reveal-modal">&#215;</a>';
 
-	$('.reveal-modal').append(closingHTML).appendTo(document.body);
+		$('.reveal-modal').append(closingHTML).appendTo(document.body);
 
-}); // Short and sweet
+	});
+})(jQuery); // Short and sweet
